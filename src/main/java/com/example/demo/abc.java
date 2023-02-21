c30b53c1285b5d76ac1a48a340165cd21fd72914
prasannakumar413 <pk8531534@gmail.com>
2023-02-21 12:14:13 +0530

    first commit

---
+++ src/main/java/com/example/demo/abc.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class abc {
+	
+	@RequestMapping("/")
+	public String display(HttpServletRequest req)
+	{
+		String name=req.getParameter("name");
+		HttpSession session = req.getSession();
+		
+session.setAttribute("displayName", name);
+		
+		return "NewFile.jsp";
+	}
+
+}
